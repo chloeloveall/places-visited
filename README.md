5af51d7d649d06334c4d582d4942d191bde3057a
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-25 17:06:18 -0800

    Write Scrapbook test and pass. Write test for addDestination prototype
    Co-Authored-by: Kerry Lang <klang812@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-1. Describe: addPlace() Test: "It creates an object with all the information given" Expect: new Place(place, year, landmark).toEqual(Place: [place, year, landmark]);
+1. Describe: addScrapbook() 
+Test: 'Create a Scrapbook acting as a mock database to hold all of instances Destinations'
+Expect(Scrapbook).toEqual(this.Destinations)
 
+Describe: addDestination()
+Test: "It contains a constructor (our Destination constructor) to hold all the keys that will hold values" 
+Expect: new Destination(location, landmark, timeOfYear).toEqual(Destination: [location, landmark, timeOfYear]);
 
+Describe: 
+Test: 'It will create a prototype method to return an activity based on the season'
+Expect(camping).toEqual(this.timeOfYear = this.timeOfYear + "string")
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+// Business logic for Scrapbook
+function Scrapbook() {
+  this.destinations = {};
+}
+
+Scrapbook.prototype.addDestination = function(destination) {
+  this.destinations[destination.location] = destination;
+} 
+
+// Business logic for Destinations
 function Destination(location, landmark, timeOfYear) {
   this.location = location;
   this.landmark = landmark;
   this.timeOfYear = timeOfYear;
 }
 
-Destination.prototype.camping = function() {
+Destination.prototype.activity = function() {
   if (this.timeOfYear === 'Summer') {
     return this.timeOfYear + ' is time to camp!';
   } else if (this.timeOfYear === 'Autumn' || this.timeOfYear === 'Fall') {
@@ -16,6 +26,8 @@ Destination.prototype.camping = function() {
   }
 }
 
+
+let scrapbook = new Scrapbook();
 let portland = new Destination('Portland', 'Mount Tabor', 'Summer');
 let seattle = new Destination('Seattle', 'Space Needle', 'Autumn');
 let bellingham = new Destination('Bellingham', 'Lake Whatcom', 'Winter');
